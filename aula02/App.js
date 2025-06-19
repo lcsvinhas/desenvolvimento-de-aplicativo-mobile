e25587c6ad6db05cc9e9c8f5f70b25a686398344
lcsvinhas <l.vinhas.lv@gmail.com>
2025-06-18 22:19:22 -0300

    criei aula02

---
+++ aula02/App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
